ba40776384b5bb4ce790da366f9f69383fd5c88f
679200 <reyna.sk.07@gmail.com>
2024-02-23 11:57:34 +0000

    booking,homepage and a lot more

---
+++ PetPals 7.0/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace k1j_prototype.Hubs
+{
+    public class ChatHub: Hub
+    {
+        public async Task SendMessage(string fromUser,string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", fromUser, message);
+        }
+    }
+}
--- PetPals 7.0/wwwroot/js/site.js
+++ PetPals 7.0/wwwroot/js/site.js
@@ -1,4 +1,16 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
-// Write your JavaScript code.
+connection.on("ReceiveMessage", function (fromUser, message) {
+    var msg = fromUser + ":" + message;
+    var li = document.createElement("li");
+    li.textContent = msg;
+    $("#list").prepend(li);
+});
+
+connection.start();
+$("#btnSend").on("click", function () {
+    var fromUser = $("#txtUser").val();
+    var message = $("#txtMsg").val();
+
+    connection.invoke("SendMessage", fromUser, message);
+});
